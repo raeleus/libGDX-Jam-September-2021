250757822b3011050bffbbba9031bf90dc9e65ca
raeleus <raymond.ray3k@gmail.com>
2021-09-26 16:54:08 -0700

    Update starting data.

--- core/src/main/java/com/ray3k/template/SaveData.java
+++ core/src/main/java/com/ray3k/template/SaveData.java
@@ -3,14 +3,14 @@ package com.ray3k.template;
 import com.ray3k.template.entities.SoldierEntity.*;
 
 public class SaveData {
-    public SoldierType[] types = {SoldierType.ASSAULT, SoldierType.SNIPER, SoldierType.HEAVY, SoldierType.ASSAULT};
+    public SoldierType[] types = {SoldierType.MILITIA, null, null, null};
     public int coins = 0;
-    public int level = 15;
+    public int level = 1;
     public int reinforcedLevel = 0;
-    public int[] moveSpeed = {2, 0, 0, 0};//assault
-    public int[] damage = {0, 4, 0, 0};//sniper
-    public int[] range = {0, 2, 0, 0};//sniper
-    public int[] health = {2, 0, 0, 2};//assault
+    public int[] moveSpeed = {0, 0, 0, 0};//assault
+    public int[] damage = {0, 0, 0, 0};//sniper
+    public int[] range = {0, 0, 0, 0};//sniper
+    public int[] health = {0, 0, 0, 0};//assault
     public int[] splash = {0, 0, 0, 0};//heavy
-    public int[] squadSize = {0, 0, 4, 0};//heavy
+    public int[] squadSize = {0, 0, 0, 0};//heavy
 }
